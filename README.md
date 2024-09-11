4ef6ed2eb74218cc86f1dcc33d4d6d885c0c117c
Zichun (Jerry) Gao <zichungao88@gmail.com>
2024-09-10 22:57:45 -0500

    Start Project 1

---
+++ README.md
@@ -0,0 +1,3 @@
+Fall 2024 ESE 2180 Project #1
+
+Team Members: Jerry Gao, Mike Gao
---
+++ project1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import json
+
+# TODO: Write a system of equations Ax = b to solve for the node voltages
+
+
+# TODO: Write a function that reads in a file to obtain the resistances on each link
+
+
+# TODO: Write a function that reads in a file to obtain the set of voltages at fixed nodes
+
+
+# TODO: Compute the matrix A using the data read in previously
+
+
+# TODO: Compute the LU factorization of A
+
+
+# TODO: Compute & output the node voltages & currents through each link
+
+
+# TODO: Write the output of the previous three steps to a file
+
+
+# TODO: Repeat but with a tree/graph network
