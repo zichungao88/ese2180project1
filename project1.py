f7751ae76bbb390a1beffeb2d2a90c08a590684f
Zichun (Jerry) Gao <zichungao88@gmail.com>
2024-09-15 17:16:17 -0500

    Complete steps 2 & 3

--- project1.py
+++ project1.py
@@ -2,12 +2,47 @@ import numpy as np
 import json
 
 # TODO: Write a system of equations Ax = b to solve for the node voltages
+# QUESTION: 5x5 or 25x25
+rows = 5
+columns = 5
+A = np.array([[1 for j in range(columns)] for i in range(rows)])
+# print(np.matrix(A))
+b = np.array([[1 for j in range(1)] for j in range(columns)])
+# print(np.matrix(b))
+# print(A.dot(b))
 
 
 # TODO: Write a function that reads in a file to obtain the resistances on each link
+# DONE
+def read_resistances_json(file_name):
+    resistances = {}
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+        for entry in data:
+            node1 = entry['node1']
+            node2 = entry['node2']
+            resistance = entry['resistance']
+            resistances[(node1, node2)] = resistance
+    return resistances
+
+resistance = read_resistances_json('node_resistances.json')
+# print(resistance)
 
 
 # TODO: Write a function that reads in a file to obtain the set of voltages at fixed nodes
+# DONE
+def read_voltages_json(file_name):
+    voltages = {}
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+        for entry in data:
+            node = entry['node']
+            voltage = entry['voltage']
+            voltages[node] = voltage
+    return voltages
+
+voltage = read_voltages_json('node_voltages.json')
+# print(voltage)
 
 
 # TODO: Compute the matrix A using the data read in previously
